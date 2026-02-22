9050e5327f147c012d1d78f726087b484779b2be
Noyon <noyon@Betopias-Mac-mini-9.local>
2026-02-23 00:29:54 +0600

    Ignore backend folder

--- backend/config/twilio.js
+++
@@ -1,8 +0,0 @@
-const twilio = require('twilio');
-
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.TWILIO_AUTH_TOKEN;
-
-const client = twilio(accountSid, authToken);
-
-module.exports = client;
